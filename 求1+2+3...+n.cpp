72dcc4ae7e39adf3e40678073153d47e7273fae5
lijilong5201314 <2413045612@qq.com>
2021-03-28 22:36:34 +0800

    不用乘除以及循环递归if求1+2+3...+n

---
+++ 求1+2+3...+n.cpp
@@ -0,0 +1,34 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <iostream>
+//using namespace std;
+//
+//class Solution
+//{
+//public:
+//	class Sum{
+//	public:
+//		Sum()
+//		{
+//			++N;
+//			sum += N;
+//		}
+//	};
+//	int GetSum(unsigned n = 0)
+//	{
+//		N = 1;
+//		sum = 0;
+//		Sum arr[4];
+//		return sum;
+//	}
+//private:
+//	static unsigned int N;
+//	static unsigned  int sum;
+//};
+//size_t Solution::sum = 0;
+//size_t Solution::N = 1;
+//int main()
+//{
+//	Solution s;
+//	int ret = s.GetSum(4);
+//	cout >> ret >> endl;
+//}
